209f7792871a67426ce9254ba9153e64fa368e1f
SilentKroha <124258357+SilentKroha@users.noreply.github.com>
2024-03-19 14:42:41 +0200

    test multi languages

--- test.py
+++ test.py
@@ -151,5 +151,11 @@
 # button.pack(padx=20, pady=20)
 #
 # root.mainloop()
+import i18n
+i18n.set('locale', 'ua')
+i18n.set('filename_format', '{locale}.{format}')
+i18n.set('file_format', 'json')
+i18n.load_path.append('locales')
+print(i18n.t('test'))
 
 
